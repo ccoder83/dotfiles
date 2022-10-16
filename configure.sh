f82d351caa2ba4c2bb7c9c0b15ed00a4b8af88a4
ccoder83 <ateen83@yahoo.com>
2022-10-16 20:09:51 +0100

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -10,7 +10,8 @@ brewInstall () {
         if test "$(uname)" = "Darwin"
         then
             /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-            echo 'eval $(/opt/homebrew/bin/brew shellenv)' >> $HOME/.zprofile
+	    echo '# Set PATH, MANPATH, etc., for Homebrew.' >> $HOME/.zprofile
+    	    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
             eval "$(/opt/homebrew/bin/brew shellenv)"
             success 'brew installed'
         elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
